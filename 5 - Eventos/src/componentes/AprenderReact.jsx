053a543889c38a6eee335c8378904f29f7a6a718
brumab <brunomolinasouza1986@gmail.com>
2025-09-12 22:19:07 -0300

    Adiciona exemplo de componente funcional

---
+++ 5 - Eventos/src/componentes/AprenderReact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+ const AprenderReact = () => {
+    return (
+        <>
+           <div>AprenderReact</div>
+           <h2>Teste</h2>
+
+        </>
+    )
+}
+
+export default AprenderReact
+
