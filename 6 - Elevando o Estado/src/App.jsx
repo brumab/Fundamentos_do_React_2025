735baeae9648d04cbcafce85d598533fe293f3e9
brumab <brunomolinasouza1986@gmail.com>
2025-09-12 22:22:37 -0300

    Adiciona exemplol

---
+++ 6 - Elevando o Estado/src/App.jsx
@@ -0,0 +1,23 @@
+import OlaMundo from "./componentes/OlaMundo"
+import AprenderReact from "./componentes/AprenderReact"
+import AprenderJSX from "./componentes/AprenderJSX"
+import AprenderAderescos from "./componentes/AprenderAderecos"
+import AprenderEventos from "./componentes/AprenderEventos"
+
+function App() {
+
+  return (
+    <>
+      <h1>Componente de aplicativo </h1>
+      {/* <OlaMundo /> */}
+      {/* <AprenderReact /> */}
+      {/* <AprenderJSX /> */}
+      {/*<AprenderAderecos stock="ubuntu" />*/}
+      <AprenderEventos />
+      
+
+    </>
+  )
+}
+
+export default App
