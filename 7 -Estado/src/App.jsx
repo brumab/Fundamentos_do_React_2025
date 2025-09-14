9e081adfaec673e58e395e06fba6351f420cdcb5
brumab <brunomolinasouza1986@gmail.com>
2025-09-14 12:05:54 -0300

    Adiciona exemplol

---
+++ 7 -Estado/src/App.jsx
@@ -0,0 +1,35 @@
+import OlaMundo from "./componentes/OlaMundo"
+import AprenderReact from "./componentes/AprenderReact"
+import AprenderJSX from "./componentes/AprenderJSX"
+import AprenderAderescos from "./componentes/AprenderAderecos"
+import AprenderEventos from "./componentes/AprenderEventos"
+import AprenderElevandoEstado from "./componentes/AprenderElevandoEstado"
+import AprenderEstado from "./componentes/AprenderEstado"
+
+
+
+
+
+function App() {
+
+  const getStock = (data) => {
+    console.log(data)
+  }
+
+  return (
+    <>
+      <h1>Componente de aplicativo </h1>
+      {/* <OlaMundo /> */}
+      {/* <AprenderReact /> */}
+      {/* <AprenderJSX /> */}
+      {/*<AprenderAderecos stock="ubuntu" />*/}
+      {/*<AprenderEventos />*/}
+      {/*<AprenderElevandoEstado getStock={getStock} />*/}
+      <AprenderEstado />
+      
+
+    </>
+  )
+}
+
+export default App
